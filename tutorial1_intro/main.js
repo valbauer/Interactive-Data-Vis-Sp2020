a29e45bacd6a04f65f192a4205db14b3f7dc2019
Valerie Bauer <valeriebauer@Valeries-MacBook-Pro-2.local>
2020-02-13 19:35:47 -0500

    fix data path

--- tutorial1_intro/main.js
+++ tutorial1_intro/main.js
@@ -1,5 +1,5 @@
 // load in csv
-d3.csv("../../data/Data73200_Tutorial1.csv").then(data => {
+d3.csv("Data73200_Tutorial1.csv").then(data => {
     // once the data loads, console log it
     console.log("data", data);
   
