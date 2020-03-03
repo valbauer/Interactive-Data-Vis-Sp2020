e8cd514efabc36620ab4ba8ad0fc0aca69d03ff7
Valerie Bauer <valeriebauer@Valeries-MacBook-Pro-2.local>
2020-03-02 21:40:13 -0500

    finished tutorial 5

--- tutorial5_geographic/main.js
+++ tutorial5_geographic/main.js
@@ -3,7 +3,8 @@
  * */
 const width = window.innerWidth * 0.9,
   height = window.innerHeight * 0.7,
-  margin = { top: 20, bottom: 50, left: 60, right: 40 };
+  margin = { top: 20, bottom: 50, left: 60, right: 40 },
+  radius = 4;
 
 /** these variables allow us to access anything we manipulate in
  * init() but need access to in draw().
@@ -14,7 +15,16 @@ let svg;
  * APPLICATION STATE
  * */
 let state = {
-  // + SET UP STATE
+  geojson: null,
+  extremes: null,
+  geojsonHover: {
+    Latitude: null,
+    Longitude: null,
+    State: null,
+  },
+  extremesHover: {
+    numDays: null
+  }
 };
 
 /**
@@ -22,10 +32,12 @@ let state = {
  * Using a Promise.all([]), we can load more than one dataset at a time
  * */
 Promise.all([
-  d3.json("PATH_TO_YOUR_GEOJSON"),
-  d3.csv("PATH_TO_ANOTHER_DATASET", d3.autoType),
-]).then(([geojson, otherData]) => {
+  d3.json("../data/us-state.json"),
+  d3.csv("../data/usHeatExtremes.csv", d3.autoType),
+]).then(([geojson, extremes]) => {
   // + SET STATE WITH DATA
+  state.geojson = geojson;
+  state.extremes = extremes;
   console.log("state: ", state);
   init();
 });
@@ -43,10 +55,63 @@ function init() {
     .attr("height", height);
 
   // + SET UP PROJECTION
+  const projection = d3.geoAlbersUsa().fitSize([width, height], state.geojson);
+  const path = d3.geoPath().projection(projection);
+
   // + SET UP GEOPATH
+  svg
+    .selectAll(".state")
+    .data(state.geojson.features)
+    .join("path")
+    .attr("d", path)
+    .attr("class", "state")
+    .attr("fill", "transparent")
+    .on("mouseover", d => {
+      // when the mouse rolls over this feature, do this
+      state.geojsonHover["State"] = d.properties.NAME;
+      draw(); // re-call the draw function when we set a new hoveredState
+    });
 
   // + DRAW BASE MAP PATH
-  // + ADD EVENT LISTENERS (if you want)
+  //console.log(d3.max(state.extremes, d => d["Change in 95 percent Days"]))
+  
+  const radiusScale = d3
+    .scaleSqrt()
+    .domain([0, 55])
+    .range([0,20]);
+
+  const dot = svg
+    .selectAll("circle")
+    .data(state.extremes, d => d)
+    .join("circle")
+    .attr("class", "extremes")
+    .attr("fill", d => {
+      if (d["Change in 95 percent Days"] > 0) {
+      return "#C70039"
+      } else { 
+        return "#49A7D3"
+      }
+    })
+    .attr("r", d => radiusScale(Math.abs(d["Change in 95 percent Days"])))
+    .attr("transform", d => {
+      const [x, y] = projection([d.Long, d.Lat]);
+      return `translate(${x}, ${y})`;
+    })
+    .on("mouseover", d => {
+      // when the mouse rolls over this feature, do this
+      state.extremesHover["Change in 95 percent Days"] = d["Change in 95 percent Days"];
+      draw();
+    })
+
+  svg.on("mousemove", () => {
+    // we can use d3.mouse() to tell us the exact x and y positions of our cursor
+    const [mx, my] = d3.mouse(svg.node());
+    // projection can be inverted to return [lat, long] from [x, y] in pixels
+    const proj = projection.invert([mx, my]);
+    state.geojsonHover["Longitude"] = proj[0];
+    state.geojsonHover["Latitude"] = proj[1];
+    draw();
+  }); 
 
   draw(); // calls the draw function
 }
@@ -55,4 +120,36 @@ function init() {
  * DRAW FUNCTION
  * we call this everytime there is an update to the data/state
  * */
-function draw() {}
+function draw() {
+
+  // return an array of [key, value] pairs
+
+  geojsonHoverData = Object.entries(state.geojsonHover);
+  extremesHoverData = Object.entries(state.extremesHover);
+
+  d3.select("#hover-content")
+    .selectAll("div.row")
+    .data(geojsonHoverData)
+    .join("div")
+    .attr("class", "row")
+    .html(
+      d =>
+        // each d is [key, value] pair
+        d[1] // check if value exist
+          ? `${d[0]}: ${d[1]}` // if they do, fill them in
+          : null // otherwise, show nothing
+    );
+  d3.select("#hover-content")
+    .selectAll("g")
+    .data(extremesHoverData)
+    .join("g")
+    .attr("class", "extremesText")
+    .html(
+      d =>
+        // each d is [key, value] pair
+        d[1] // check if value exist
+          ? `${d[0]}: ${d[1]}` // if they do, fill them in
+          : null // otherwise, show nothing
+  );
+  
+}
